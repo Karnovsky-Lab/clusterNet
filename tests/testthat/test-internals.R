7fb9308805039e833010683c0697ec381139f5ac
chrispatsalis <patsalis@umich.edu>
2023-03-22 17:25:39 -0400

    updated unit tests to reflect changes associated to custom s3 object

--- tests/testthat/test-internals.R
+++ tests/testthat/test-internals.R
@@ -1,8 +1,13 @@
 test_that("run_consensus_cluster works",{
 
-  expect_equal(run_consensus_cluster(readRDS(test_path("testdata", "test-inputNetwork.rds")),
-                                     tau = 0.5, main.seed = 417)$dcl,
-               readRDS(test_path("testdata","test-consensus_membership.rds")))
+
+  test_output <- run_consensus_cluster(readRDS(test_path("testdata", "test-inputNetwork.rds")),
+                                       tau = 0.5, main.seed = 417)
+
+  #tests the subnetwork output
+  expect_equal(test_output$dcl, readRDS(test_path("testdata","test-consensus_membership.rds")))
+  #really is testing that get_consensus_matrix is working properly
+  expect_equal(test_output$D, readRDS(test_path("testdata", "test-D_adjacency_matrix.rds")))
 })
 
 test_that("gatherConsensus_Matrix works",{
